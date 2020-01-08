9899395d7ffede8c5096d59e591b1a3c18e611c4
ViktorPetreski <vpetreski96@gmail.com>
2020-01-08 23:37:50 +0100

    change openapi server

--- api/src/main/java/com/fri/code/ide/api/v1/CodeIDEApplication.java
+++ api/src/main/java/com/fri/code/ide/api/v1/CodeIDEApplication.java
@@ -4,13 +4,14 @@ import com.kumuluz.ee.discovery.annotations.RegisterService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
-@OpenAPIDefinition(info = @Info(title = "IDEApi", version = "v1.0.0"), servers = @Server(url = "http://34.67.123.142/script/v1"), security = @SecurityRequirement(name = "openid-connect"))
+@OpenAPIDefinition(info = @Info(title = "IDEApi", version = "v1.0.0"), servers = @Server(url = "http://34.70.146.92:8080/v1"), security = @SecurityRequirement(name = "openid-connect"))
 @RegisterService
 public class CodeIDEApplication extends Application {
 }
